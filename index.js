c2abd7c3e1d84f50a9d138bc97d7fb3f766ef90c
VARUN <varunkumarmedam@gmail.com>
2018-12-01 21:07:29 +0530

    hello

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
  
  app.get('/', function(req, res){
 	res.send("Hell Wolrd");
  	res.end("Done :)");
+ 	console.log("Server started successfully");
  })
 
  app.listen(port);
