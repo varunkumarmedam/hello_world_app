3847b7ab8cebbce88012ae12c36b98b67cd28f69
VARUN <varunkumarmedam@gmail.com>
2018-12-02 15:06:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var app = express();
 var port = process.env.PORT || 80;
  
  app.get('/', function(req, res){
-	res.send("Hello Wolrd");
- 	res.end("Done :)");
  	console.log("Server started successfully");
+	res.send("Hello Wolrd");
  })
 
  app.listen(port);
